8272334b8a831066b8ed1793ce9275d7411ec9fd
Raviranjan Pathak <raviranjanpathak750@gmail.com>
2026-03-21 00:02:35 +0530

    fixed folder structure

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const section = document.getElementById(targetId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
